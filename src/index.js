0d3a23d8ae172f0e0c2a6fe3d964106edffaaebe
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-06-30 18:11:03 -0600

    index.js changed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Resolutions from "./components/Resolutions";
+import NotFoundRoute from "./routes/NotFoundRoute";
+import ProfileRoute from "./routes/ProfileRoute";
+import EditResolution from "./components/EditResolution";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index path="resolutions" element={<Resolutions />} />
+        <Route path="resolutions/:resolutionId" element={<EditResolution />} />
+        <Route path="profile" element={<ProfileRoute />} />
+        <Route path="*" element={<NotFoundRoute />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
