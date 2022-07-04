21350efd1250d9884790f53b31b2d44003ca3a77
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2022-07-03 22:49:27 -0600

    at least 5 tests passing

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,6 +1,6 @@
 import { hexToRGB, timeAgo } from "./utils";
 
-describe("utils functions should work", () => {
+describe("hexToRGB fn", () => {
   test("hexToRGB fn should return a css rgba function", () => {
     const alpha = 0.5;
     const answer = hexToRGB("#000000", alpha);
@@ -11,24 +11,38 @@ describe("utils functions should work", () => {
     const answer = hexToRGB("#000000");
     expect(answer).toBe(`rgb(0, 0, 0)`);
   });
+});
 
-  test("timeAgo fn should return seconds ago", () => {
+describe("time ago function", () => {
+  test("should return seconds ago", () => {
     const now = new Date();
     const twentySecondsAgo = now.getTime() - 20 * 1000;
     const answer = timeAgo(new Date(twentySecondsAgo));
     expect(answer).toBe("20 seconds ago");
   });
 
-  test("timeAgo fn should return minutes ago", () => {
+  test("should return minutes ago", () => {
     const now = new Date();
     const threeMinutesAgo = now.getTime() - 3 * 60 * 1000;
     const answer = timeAgo(new Date(threeMinutesAgo));
     expect(answer).toBe("3 minutes ago");
   });
 
-  test.todo("timeAgo fn should return days ago");
+  test("should return hours ago", () => {
+    const now = new Date();
+    const threeMinutesAgo = now.getTime() - 4 * 60 * 60 * 1000;
+    const answer = timeAgo(new Date(threeMinutesAgo));
+    expect(answer).toBe("4 hours ago");
+  });
 
-  test.todo("timeAgo fn should return months ago");
+  test("should return days ago", () => {
+    const now = new Date();
+    const threeMinutesAgo = now.getTime() - 2 * 24 * 60 * 60 * 1000;
+    const answer = timeAgo(new Date(threeMinutesAgo));
+    expect(answer).toBe("2 days ago");
+  });
 
-  test.todo("timeAgo fn should return years ago");
+  test.todo("should return months ago");
+
+  test.todo("should return years ago");
 });
